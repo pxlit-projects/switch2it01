94fa429775990c2915d3daa4604e8d6db59bb718
philippeVDB <philippe.vandebosch1@gmail.com>
2015-05-11 14:22:05 +0200

    Added 9 base classes + enquerycontroller

---
+++ FinahProject/Models/ExtraPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinahProject.Models
+{
+    public class ExtraPerson
+    {
+        private String nameValue;
+
+        public String Name
+        {
+            get { return nameValue; }
+            set { nameValue = value; }
+        }
+        
+    }
+}
---
+++ FinahProject/Models/Subject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinahProject.Models
+{
+    public class Subject
+    {
+        private String nameValue;
+        private String descriptionValue;
+
+        public String Description
+        {
+            get { return descriptionValue; }
+            set { descriptionValue = value; }
+        }
+        
+
+        public String Name
+        {
+            get { return nameValue; }
+            set { nameValue = value; }
+        }
+        
+    }
+}
